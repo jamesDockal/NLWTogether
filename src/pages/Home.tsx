f7357070916c6ad25b89827a78d2be7522f0014b
james dockal <jamesistranho@gmail.com>
2021-06-22 15:26:41 -0300

    first commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import illustration from "../assets/illustration.svg";
+
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <aside>
+        <img src={illustration} />
+      </aside>
+    </div>
+  );
+}
